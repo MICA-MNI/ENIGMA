771b872632bd4795cfd00d4958b83ff155742af0
saratheriver <saratheriver@gmail.com>
2020-07-16 15:09:42 -0400

    mapping

--- matlab/shared/surfstat/parcels_to_vertices.m
+++ matlab/shared/surfstat/parcels_to_vertices.m
@@ -1,4 +1,4 @@
-function map2surf = parcels_to_vertices(parcel_vector, label_vector, surface_name)
+function map2surf = parcels_to_vertices(parcel_data, surface_name)
 %
 % map2surf = parcels_to_vertices(parcel_vector, surface, label_vector)
 %
@@ -21,15 +21,17 @@ end
 
 % load surfaces
 if strcmp(surface_name, 'fsa5')
-    surf = SurfStatAvSurf({'fsa5_lh', 'fsa5_rh'});
+    surf         = SurfStatAvSurf({'fsa5_lh', 'fsa5_rh'});
+    label_vector = dlmread('aparc_fsa5.csv'); 
 elseif strcmp(surface_name, 'conte69')
     surf = SurfStatAvSurf({'conte69_lh', 'conte69_rh'});
+    label_vector = dlmread('aparc_conte69.csv'); 
 end
 
 map2surf = zeros(1,size(surf.coord,2)); 
 uparcel = unique(label_vector);
 for i=1:length(label_vector)
     index = label_vector==uparcel(i);
-    map2surf(index) = parcel_vector(i); 
+    map2surf(index) = parcel_data(i); 
 end
 
