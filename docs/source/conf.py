9f111b245b03ae875a3b842bd644f464008082a8
saratheriver <saratheriver@gmail.com>
2020-07-16 23:18:23 -0400

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,7 @@ html_static_path = ['_static']
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
-html_css_files = ['css/saratheriver_enigma.css']
+html_css_files = ['css/saratheriver_enigma.css', 'saratheriver_tabs.css']
 
 #html_style = 'css/saratheriver_enigma.css'
 
