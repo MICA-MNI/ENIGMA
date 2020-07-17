e57d27528872b3cb9ea64c0dbd9222164629cee6
saratheriver <saratheriver@gmail.com>
2020-07-17 00:07:52 -0400

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -70,4 +70,4 @@ html_css_files = ['css/saratheriver_enigma.css']
 # add custom files that are stored in _static
 def setup(app):
    app.add_stylesheet("css/saratheriver_tabs.css")
-   app.add_stylesheet("css/saratheriver_friendly.css")
+   app.add_stylesheet("css/saratheriver_friendly.py")
