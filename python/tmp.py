121cbdf19592c2aa6e1e88d7e184da1e14abcb86
saratheriver <saratheriver@gmail.com>
2020-07-26 12:57:57 -0400

    docs

---
+++ python/tmp.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class struct():
+  pass
+
+sc  = struct()
+sc.labels_sctx = ('ACCUMBENS_LEFT',	'AMYGDALA_LEFT',	'CAUDATE_LEFT',	'HIPPOCAMPUS_LEFT')
+sc.sctx = np.array([[1, 4, 5], [-5, 8, 9]])
