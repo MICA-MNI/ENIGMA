b575fd1cf8d473e0a11330d9ba51b55b8044ed5e
saratheriver <saratheriver@gmail.com>
2020-07-28 12:48:48 -0400

    rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'ENIGMA TOOLBOX'
-copyright = '2020, Sara Lariviere, Raul-Rodriguez-Cruces, Bo-Yong Park, Boris Bernhardt'
+copyright = '2020, enigmators'
 author = 'Sara Lariviere, Raul-Rodriguez-Cruces, Bo-Yong Park, Boris Bernhardt'
 
 
