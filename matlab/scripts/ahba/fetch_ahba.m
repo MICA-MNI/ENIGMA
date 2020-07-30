282a5a7f76f9bbb541b630ebd80ab4a9375b79b6
Sara Lariviere <saratheriver@gmail.com>
2020-07-30 12:25:47 -0400

    [ENH] fetch ahba locally using wget

--- matlab/scripts/ahba/fetch_ahba.m
+++ matlab/scripts/ahba/fetch_ahba.m
@@ -1,8 +1,16 @@
-function [gx, reglabels, genelabels] = fetch_ahba()
+function [gx, reglabels, genelabels] = fetch_ahba(csvfile)
 %
-% Usage: [gx, reglabels, genelabels] = fetch_ahba()
+% Usage: [gx, reglabels, genelabels] = fetch_ahba();
 %
 % Simple script to fetch microarray expression data (pre-computed)
+%   Input (OPTIONAL):
+%       If the allgenes.csv file is already downloaded locally, then
+%       specific its path as follows: 
+%       [gx, reglabels, genelabels] = fetch_ahba('/path/to/allgenes.csv');
+%
+%       Leave empty to fetch data file from the internet (requires a good
+%       connection!)
+%
 %   Outputs:
 %       gx          = matrix of gene expression data (82 x 15633)
 %       reglabels   = name of cortical regions in same order as gx
@@ -19,25 +27,33 @@ function [gx, reglabels, genelabels] = fetch_ahba()
 %
 % Last modifications:
 % SL | a rainy July day 2020
+% SL | the day after the rainy day (it's now sunny)
 
-% Fetch the csv table from github and load it locally | option 1
-% url = 'https://raw.githubusercontent.com/saratheriver/enigma-extra/master/ahba/allgenes.csv';
-% urlwrite(url, '.gtmp.csv');
-% g = readtable('.gtmp.csv');
+if nargin < 1
+    % Fetch the csv table from github and load it locally | option 1
+    % url = 'https://raw.githubusercontent.com/saratheriver/enigma-extra/master/ahba/allgenes.csv';
+    % urlwrite(url, '.gtmp.csv');
+    % g = readtable('.gtmp.csv');
 
-% Fetch the csv table from github and load it locally | option 2
-!wget https://raw.githubusercontent.com/saratheriver/enigma-extra/master/ahba/allgenes.csv
-g = readtable('allgenes.csv');
+    % Fetch the csv table from github and load it locally | option 2
+    !wget https://raw.githubusercontent.com/saratheriver/enigma-extra/master/ahba/allgenes.csv
+    g = readtable('allgenes.csv');
 
-% Extract relevant information
-gx          = table2array(g(:, 2:end));
-reglabels   = table2array(g(:, 1));
-genelabels  = g.Properties.VariableNames(2:end);
+    % Extract relevant information
+    gx          = table2array(g(:, 2:end));
+    reglabels   = table2array(g(:, 1));
+    genelabels  = g.Properties.VariableNames(2:end);
 
-% remove table locally | option 1
-% delete('.gtmp.csv');
+    % remove table locally | option 1
+    % delete('.gtmp.csv');
 
-% remove table locally | option 2
-delete('allgenes.csv');
-
+    % remove table locally | option 2
+    delete('allgenes.csv');
+else
+    g = readtable(csvfile);
+    gx          = table2array(g(:, 2:end));
+    reglabels   = table2array(g(:, 1));
+    genelabels  = g.Properties.VariableNames(2:end);
+end
+    
 return
