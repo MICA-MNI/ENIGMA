5c3ab2217800fb648ea94330fc1578610938b811
saratheriver <saratheriver@gmail.com>
2020-07-31 16:01:32 -0400

    [ENH] get disease risk genes

--- enigmatoolbox/datasets/__init__.py
+++ enigmatoolbox/datasets/__init__.py
@@ -1,7 +1,7 @@
 from .base import (load_conte69, load_mask, load_sc, load_fc,
                    load_parcellation, load_marker, load_fsa5,
                    load_subcortical, enigma_covariance,
-                   fetch_ahba, epilepsy_genes)
+                   fetch_ahba, risk_genes)
 
 __all__ = ['load_conte69',
            'load_fsa5',
@@ -13,4 +13,4 @@ __all__ = ['load_conte69',
            'load_fc',
            'enigma_covariance',
            'fetch_ahba',
-           'epilepsy_genes']
+           'risk_genes']
