69df5f357f3e503ab7bd6580f3962f68a4ed757b
saratheriver <saratheriver@gmail.com>
2020-10-05 16:57:25 -0400

    [ENH 🔧] name-value pairs for surface viewers

--- matlab/scripts/surface_viewer/plot_cortical.m
+++ matlab/scripts/surface_viewer/plot_cortical.m
@@ -1,38 +1,47 @@
-function [a,cb]=plot_cortical(data, surface_name, title, background);
+function [a,cb]=plot_cortical(data, varargin);
 
-% plot_cortical(data, surf, title, background);
+% plot_cortical(data, varargin);
 % 
-% Usage: [a,cb] = plot_cortical(data, surf[, title[, background]]);
+% Usage: [a,cb] = plot_cortical(data, varargin);
 % 
-% data            = 1 x v vector of data, v=#vertices
-% surface_name    = 'fsa5' (default) or 'conte69'
-% title           = any string, data name by default.
-% background      = background colour, any matlab ColorSpec, such as 
-%   'white' (default), 'black'=='k', 'r'==[1 0 0], [1 0.4 0.6] (pink) etc.
+% INPUT
+%   data            = 1 x v vector of data, v=#vertices
+%
+% OPTIONAL INPUTS
+%   surface_name    = 'fsa5' (default) or 'conte69'
+%   label_text      = any string, data name by default.
+%   background      = background colour, any matlab ColorSpec, such as 
+%                     'white' (default), 'black'=='k', 'r'==[1 0 0], [1 0.4 0.6] (pink) etc.
+%   color_range     = range of colorbar, default is [min(data max(data)]
+%   cmap            = colormap name (default is viridis)
+%
 %   Letter and line colours are inverted if background is dark (mean<0.5).
 %
-% a  = vector of handles to the axes, left to right, top to bottom. 
-% cb = handle to the colorbar.
+% OUTPUTS
+%   a               = vector of handles to the axes, left to right, top to bottom. 
+%   cb              = handle to the colorbar.
 %
 % Sara Lariviere  |  saratheriver@gmail.com
 %
 % Last modifications:
 % SL | a humid July day 2020
+% SL | changed to name-value pairs on a Fall day, October 2020
 
-if nargin<2
-    surface_name = 'fsa5';
-end
-if nargin<3 
-    title=inputname(1);
-end
-if nargin<4
-    background='white';
-end
+p = inputParser;
+addParameter(p, 'surface_name', 'fsa5', @ischar);
+addParameter(p, 'label_text', "", @ischar);
+addParameter(p, 'background', 'white', @ischar);
+addParameter(p, 'color_range', [min(data) max(data)], @isnumeric);
+addParameter(p, 'cmap', 'viridis', @ischar);
+
+% Parse the input
+parse(p, varargin{:});
+in = p.Results;
 
 % load surfaces
-if strcmp(surface_name, 'fsa5')
+if strcmp(in.surface_name, 'fsa5')
     surf = SurfStatAvSurf({'fsa5_lh', 'fsa5_rh'});
-elseif strcmp(surface_name, 'conte69')
+elseif strcmp(in.surface_name, 'conte69')
     surf = SurfStatAvSurf({'conte69_lh', 'conte69_rh'});
 end
 
@@ -93,16 +102,19 @@ cb=colorbar('location','South');
 set(cb,'Position',[0.35 0.18 0.3 0.03]);
 set(cb,'XAxisLocation','bottom');
 h=get(cb,'Title');
-set(h,'String',title);
+set(h,'String', in.label_text);
 
-whitebg(gcf,background);
-set(gcf,'Color',background,'InvertHardcopy','off');
+whitebg(gcf, in.background);
+set(gcf,'Color', in.background, 'InvertHardcopy', 'off');
 
-dcm_obj=datacursormode(gcf);
-set(dcm_obj,'UpdateFcn',@SurfStatDataCursor,'DisplayStyle','window');
+dcm_obj = datacursormode(gcf);
+set(dcm_obj, 'UpdateFcn', @SurfStatDataCursor, 'DisplayStyle', 'window');
 
-%% colormaps 
-colormap(viridis)
+%% set colorbar range 
+colorbar_range(in.color_range)
+
+%% set colormaps 
+enigma_colormap(in.cmap)
 
 return
 end
--- matlab/scripts/surface_viewer/plot_subcortical.m
+++ matlab/scripts/surface_viewer/plot_subcortical.m
@@ -1,53 +1,58 @@
-function [a, cb] = plot_subcortical(data, ventricles, title, background);
+function [a, cb] = plot_subcortical(data, varargin);
 
 % plot_subcortical is a simple viewer for subcortical surface data
 % 
-% Usage: [a, cb] = plot_subcortical(data, ventricles [,title [,background]]);
+% Usage: [a, cb] = plot_subcortical(data, varargin);
 % 
-% data        = 1 x v vector of data, v=# of subcortical structures (16)
-%   one value per subcortical structure
-%   order is L-accumbens, L-amygdala, L-caudate, L-hippocampus, L-pallidum
-%               L-putamen, L-thalamus, L-ventricle, R-accumbens, R-amygdala, R-caudate,
+% INPUT
+%   data        = 1 x v vector of data, v=# of subcortical structures (16)
+%                 one value per subcortical structure
+%                   order is L-accumbens, L-amygdala, L-caudate, L-hippocampus, L-pallidum
+%                   L-putamen, L-thalamus, L-ventricle, R-accumbens, R-amygdala, R-caudate,
 %                   R-hippocampus, R-pallidum, R-putamen, R-thalamus,
 %                   R-ventricle
 %
-% ventricles  = boolean 'True' (default) shows the ventricles (data must be
-%               a 1 x 16 vector). If 'False', then ventricles are not shown
-%               and data must be a 1 x 14 vector.
+% OPTIONAL INPUTS
+%   ventricles  = boolean 'True' (default) shows the ventricles (data must be
+%                 a 1 x 16 vector). If 'False', then ventricles are not shown
+%                 and data must be a 1 x 14 vector
+%   label_text  = any string, data name by default
+%   background  = background colour, any matlab ColorSpec, such as 
+%                 'white' (default), 'black'=='k', 'r'==[1 0 0], [1 0.4 0.6] (pink) etc.
+%   color_range = range of colorbar, default is [min(data max(data)]
+%   cmap        = colormap name (default is viridis)
 %
-% title       = any string, data name by default.
-%
-% background  = background colour, any matlab ColorSpec, such as 
-%   'white' (default), 'black'=='k', 'r'==[1 0 0], [1 0.4 0.6] (pink) etc.
 %   Letter and line colours are inverted if background is dark (mean<0.5).
 %
-% a  = vector of handles to the axes, left to right, top to bottom. 
-% cb = handle to the colorbar.
+% OUTPUTS
+% a             = vector of handles to the axes, left to right, top to bottom. 
+% cb            = handle to the colorbar.
 %
 % Sara Lariviere  |  saratheriver@gmail.com
 %
 % Last modifications:
 % SL | a rainy November night 2019
 % SL | added ventricles on a sunny July 2020 morning
+% SL | changed to name-value pairs on a Fall day, October 2020
 
+p = inputParser;
+addParameter(p, 'ventricles', 'True', @ischar);
+addParameter(p, 'label_text', "", @ischar);
+addParameter(p, 'background', 'white', @ischar);
+addParameter(p, 'color_range', [min(data) max(data)], @isnumeric);
+addParameter(p, 'cmap', 'viridis', @ischar);
 
-if nargin<2
-    ventricles = 'True';
-end
-if nargin<3
-    title = inputname(1);
-end
-if nargin<4
-    background = 'white';
-end
+% Parse the input
+parse(p, varargin{:});
+in = p.Results;
 
 % load subcortical templates
 surf_lh = SurfStatReadSurf('sctx_lh');
 surf_rh = SurfStatReadSurf('sctx_rh');
 
 % super inefficient way to attribute vertices to subcortical areas
-if strcmp(ventricles, 'True')
-    data = [ repmat(data(1), 867, 1); repmat(data(2), 1419, 1); ...
+if strcmp(in.ventricles, 'True')
+    data = [repmat(data(1), 867, 1); repmat(data(2), 1419, 1); ...
             repmat(data(3), 3012, 1); repmat(data(4), 3784, 1); ...
             repmat(data(5), 1446, 1); repmat(data(6), 4003, 1); ...
             repmat(data(7), 3726, 1); repmat(data(8), 7653, 1);...
@@ -55,11 +60,11 @@ if strcmp(ventricles, 'True')
             repmat(data(11), 3208, 1); repmat(data(12), 3742, 1); ...
             repmat(data(13), 1373, 1); repmat(data(14), 3871, 1); ...
             repmat(data(15), 3699, 1); repmat(data(16), 7180, 1)];
-elseif strcmp(ventricles, 'False')
+elseif strcmp(in.ventricles, 'False')
     data1 = nan(16, 1);
     data1([1:7 9:15]) = data;
     
-    data = [ repmat(data1(1), 867, 1); repmat(data1(2), 1419, 1); ...
+    data = [repmat(data1(1), 867, 1); repmat(data1(2), 1419, 1); ...
             repmat(data1(3), 3012, 1); repmat(data1(4), 3784, 1); ...
             repmat(data1(5), 1446, 1); repmat(data1(6), 4003, 1); ...
             repmat(data1(7), 3726, 1); repmat(data1(8), 7653, 1);...
@@ -123,7 +128,7 @@ lighting phong; material dull; shading flat;
 
 for i=1:length(a)
     set(a(i),'CLim',clim);
-    set(a(i),'Tag',['SurfStatView ' num2str(i) ]);
+    set(a(i),'Tag',['SurfStatView ' num2str(i)]);
 end
 
 
@@ -131,18 +136,19 @@ cb=colorbar('location','South');
 set(cb,'Position',[0.35 0.18 0.3 0.03]);
 set(cb,'XAxisLocation','bottom');
 h=get(cb,'Title');
-set(h,'String',title);
+set(h,'String', in.label_text);
 
-whitebg(gcf,background);
-set(gcf,'Color',background,'InvertHardcopy','off');
+whitebg(gcf, in.background);
+set(gcf,'Color', in.background, 'InvertHardcopy', 'off');
 
 dcm_obj=datacursormode(gcf);
 set(dcm_obj,'UpdateFcn',@SurfStatDataCursor,'DisplayStyle','window');
 
-%% colormaps 
-% matlab original (boring)
-colormap([.7 .7 .7; parula(256)])
+%% set colorbar range 
+colorbar_range(in.color_range)
 
+%% set colormaps 
+enigma_colormap(in.cmap)
 
 return
 end
