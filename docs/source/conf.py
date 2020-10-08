3254f3fd7c777ebc15e552c19351ab9eac3840ab
saratheriver <saratheriver@gmail.com>
2020-10-08 14:57:18 -0400

    [DOC 📄] added autodoc... why.. WHYYY this no worky

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,7 @@ import os
 import sys
 import warnings
 #sys.path.insert(0, os.path.abspath('../../'))
-sys.path.append(os.path.abspath('../..'))
-sys.path.append(os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../../enigmatoolbox'))
 
 import enigmatoolbox
 
