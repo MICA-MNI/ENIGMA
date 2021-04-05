2d92ffebae0eb7fc679a3738d88fac7533f95144
saratheriver <saratheriver@gmail.com>
2021-04-05 09:57:56 -0400

    [ENH 👀] added c69 midline mask

--- enigmatoolbox/datasets/__init__.py
+++ enigmatoolbox/datasets/__init__.py
@@ -2,7 +2,7 @@ from .base import (load_conte69, load_sc, load_fc,
                    load_fsa5, load_subcortical, structural_covariance,
                    fetch_ahba, risk_genes, load_example_data,
                    load_summary_stats, load_fc_as_one, load_sc_as_one,
-                   nfaces, getaffine)
+                   nfaces, getaffine, load_mask)
 
 __all__ = ['load_conte69',
            'load_fsa5',
@@ -17,4 +17,5 @@ __all__ = ['load_conte69',
            'load_fc_as_one',
            'load_sc_as_one',
            'nfaces',
-           'getaffine']
+           'getaffine',
+           'load_mask']
